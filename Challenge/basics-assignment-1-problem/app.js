1e1be8ae19c560c6335d018c63ecda8b8b039a44
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-03-29 12:06:55 -0300

    up

--- Challenge/basics-assignment-1-problem/app.js
+++ Challenge/basics-assignment-1-problem/app.js
@@ -1,21 +1,22 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      name: "andre",
-      age: 29,
-      
-      
-    };
-   
-  },
-  computed: {
-    ageinfive(){
-    return this.age + 5;
-    }
-    },
-  
-  methods: {
-    
-  }
-  });
-app.mount("#assignment");
+	data() {
+		return {
+			name: "andre",
+			age: 29,
+			img: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		};
+	},
+	computed: {
+		ageinfive() {
+			return this.age + 5;
+		},
+	},
+
+	methods: {
+		random() {
+			return Math.round(Math.random());
+		},
+	},
+});
+
+app.mount("#assignment"); // esse selector é o msm que usamos no css, então podemos até usar classes, .assignment caso fosse classe por exemplo.
