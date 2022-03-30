f4848c1b2d318c03befd0eca3742a6821a61b70a
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-03-30 12:05:59 -0300

    30.03.22

--- 21/app.js
+++ 21/app.js
@@ -1,12 +1,29 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    
-  }
+	data() {
+		return {
+			counter: 0,
+			name: "",
+			confirmedName: "",
+		};
+	},
+	methods: {
+		confirmInput() {
+			this.confirmedName = this.name;
+		},
+		add(num) {
+			//esse num eu coloco no html e controlo por lá
+			this.counter = this.counter + num;
+		},
+		reduce(num) {
+			this.counter = this.counter - num;
+		},
+		setName(event) {
+			this.name = event.target.value;
+		},
+		submitForm() {
+			alert("submited");
+		},
+	},
 });
 
-app.mount('#events');
+app.mount("#events");
