49491ad3ed31defb2b939596de95acb92892c363
André Augusto dos Santos <andreaugustodossantos@gmail.com>
2022-04-05 11:43:05 -0300

    k13final

--- 21/app.js
+++ 21/app.js
@@ -1,29 +1,32 @@
 const app = Vue.createApp({
-	data() {
-		return {
-			counter: 0,
-			name: "",
-			confirmedName: "",
-		};
-	},
-	methods: {
-		confirmInput() {
-			this.confirmedName = this.name;
-		},
-		add(num) {
-			//esse num eu coloco no html e controlo por lá
-			this.counter = this.counter + num;
-		},
-		reduce(num) {
-			this.counter = this.counter - num;
-		},
-		setName(event) {
-			this.name = event.target.value;
-		},
-		submitForm() {
-			alert("submited");
-		},
-	},
+  data() {
+    return {
+      counter: 10,
+      name: "",
+      confirmedName: "",
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    reset() {
+      return (this.name = ""), (this.confirmedName = "");
+    },
+    add(num) {
+      //esse num eu coloco no html e controlo por lá
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    submitForm() {
+      alert("submited");
+    },
+  },
 });
 
 app.mount("#events");
